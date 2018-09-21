be989d5386394aadaac1ee9bc088aa3e56a096de
robotum <37242344+robotum@users.noreply.github.com>
2018-09-22 01:15:01 +0800

    try

--- codeitsuisse/routes/highest_occur.py
+++ codeitsuisse/routes/highest_occur.py
@@ -18,4 +18,4 @@ def check():
     i = word_count.values().index(maxi)
     result = word_count.items()[i]
     logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return jsonify(result);
