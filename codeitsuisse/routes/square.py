e9ce4648a92667c7e3f41b0ba45749d15c1e06d4
Jingyi0422 <fjy0422@gmail.com>
2018-09-21 18:52:15 +0800

    try change

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -6,12 +6,13 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
+
 @app.route('/square', methods=['POST'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue * inputValue
+    result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
