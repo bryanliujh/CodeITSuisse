daabc3f91fcf22a5e639e3885c6f739f9d5ec0c2
robotum <37242344+robotum@users.noreply.github.com>
2018-09-22 01:04:57 +0800

    try

---
+++ codeitsuisse/routes/highest_occur.py
@@ -0,0 +1,20 @@
+import logging
+
+from flask import request, jsonify;
+from collections import Counter;
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/higho', methods=['POST'])
+def check():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputString = data.get("string")
+    word_count = Counter(inputString)
+    maxi = max(word_count.values())
+    i = word_count.values().index(maxi)
+    result = word_count.items()[i]
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
