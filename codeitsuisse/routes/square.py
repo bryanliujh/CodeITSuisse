1334151813d5ffa42832544668856b036e91de2e
robotum <37242344+robotum@users.noreply.github.com>
2018-09-22 00:34:33 +0800

    a

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -13,7 +13,7 @@ def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue * inputValue
+    result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
