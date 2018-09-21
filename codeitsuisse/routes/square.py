2b41903f960bd4161de6a2d23e06d4a082dbb886
bryanliujh <bbx54ever@hotmail.com>
2018-09-21 18:55:55 +0800

    test

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -11,7 +11,7 @@ def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue * inputValue
+    result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
