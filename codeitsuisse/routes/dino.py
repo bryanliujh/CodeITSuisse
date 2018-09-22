877386ca9cb93b517b4b75ecf5501f2ed19459e5
robotum <37242344+robotum@users.noreply.github.com>
2018-09-22 16:59:33 +0800

    tally expense bug fix

--- codeitsuisse/routes/dino.py
+++ codeitsuisse/routes/dino.py
@@ -62,12 +62,10 @@ def dino():
     while counter > 0:
         for i in range(x, types):
             sum += twod_list[i][int(binary_list[bit_count])]
-            print("bit count: %s" % bit_count)
             bit_count += 1
 
         while counter2 > 0:
             for j in range(y, types*2):
-                print("another_bit_count: %s" % another_bit_count)
                 sumsum += twod_list[j][int(another_binary_list[another_bit_count])]
                 another_bit_count += 1
             if abs(sum - sumsum) <= max_diff:
