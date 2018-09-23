2a4a22b5d030606c2f7b9daa1d72dbdf46fa07fa
Jingyi0422 <fjy0422@gmail.com>
2018-09-23 11:09:42 +0800

    bug fix

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -8,3 +8,4 @@ import codeitsuisse.routes.tally_expense
 import codeitsuisse.routes.air_traffic_controller
 import codeitsuisse.routes.image_gps
 import codeitsuisse.routes.dino
+import codeitsuisse.routes.minimum_camps
